fcedaef764e038591697f87ad07972b7eb32cced
Murphey Osmundson <mosmundson88@gmail.com>
2023-07-24 22:26:24 -0400

    initial build of service models and view functions

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,160 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from common.json import ModelEncoder
+from django.views.decorators.http import require_http_methods
+from .models import Technician, AutomobileVO, Appointment
 
-# Create your views here.
+
+
+class TechnicianEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+
+class AppointmentEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        "date_time",
+        "reason",
+        "status",
+        "vin",
+        "customer"
+        "technician"
+    ]
+
+    encoders = {
+        "technician": TechnicianEncoder()
+    }
+
+
+@require_http_methods('GET', 'POST')
+def api_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians}
+        )
+    else:
+        content = json.loads(request.body)
+        technician = Technician.objects.create(**content)
+        return JsonResponse(
+            technician,
+            encoder=TechnicianEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def api_technician(request, pk):
+    if request.method == "GET":
+        try:
+            technician = Technician.objects.get(id=pk)
+            return JsonResponse(
+                technician,
+                encoder=TechnicianEncoder,
+                safe=False
+            )
+        except Technician.DoesNotExist:
+            return JsonResponse(
+                {"message": "The technician you are trying to access does not exist"}
+                status=400
+            )
+    elif request.method == "PUT":
+        try:
+            content = json.loads(request.body)
+            technician = Technician.objects.get(id=pk)
+            props = ["first_name", "last_name", "employee_id"]
+            for prop in props:
+                if prop in content:
+                    setattr(technician, prop, content[prop])
+            technician.save()
+            return JsonResponse(
+                technician,
+                encoder=TechnicianEncoder,
+                safe=False,
+            )
+        except Technician.DoesNotExist:
+            return JsonResponse(
+                {"message": "The technician you are trying to update does not exist"},
+                status=404,
+            )
+    else:
+        count, _ = Technician.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
+
+
+@require_http_methods('"GET, "POST')
+def api_appointments(request, technician_id=None):
+    if request.method == "GET":
+        if technician_id is not None:
+            appointment = Appointment.objects.filter(technician=technician_id)
+        else:
+            appointment = Appointment.objects.all()
+        return JsonResponse(
+            {"appointment": appointment},
+            encoder=AppointmentEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+
+        try:
+            technician_href = content["technician"]
+            technician = Technician.objects.get(import_href=technician_href)
+            content["technician"] = technician
+        except Technician.DoesNotExist:
+            return JsonResponse(
+                {"message": "The technician you are attempting to assign this appointment to does not exist"},
+                status=400,
+            )
+
+        appointment = Appointment.objects.create(**content)
+        return JsonResponse(
+            appointment,
+            encoder=AppointmentEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def api_appointment(request, pk):
+    if request.method == "GET":
+        try:
+            appointment = Appointment.objects.get(id=pk)
+            return JsonResponse(
+                appointment,
+                encoder=AppointmentEncoder,
+                safe=False,
+            )
+        except Appointment.DoesNotExist:
+            return JsonResponse(
+                {"message": "This appointment does not exist"},
+                status=400,
+            )
+    elif request.method == "PUT":
+        try:
+            content = json.loads(request.body)
+            appointment = Appointment.objects.get(id=pk)
+
+            props = ["date_time", "reason", "status", "vin", "customer", "technician"]
+            for prop in props:
+                if prop in content:
+                    setattr(appointment, prop, content[prop])
+            appointment.save()
+            return JsonResponse(
+                appointment,
+                encoder=AppointmentEncoder,
+                safe=False,
+            )
+        except Appointment.DoesNotExist:
+            return JsonResponse(
+                {"message": "The appointment you are trying to update does not exist"},
+                status=404,
+            )
+    else:
+        count, _ = Appointment.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
