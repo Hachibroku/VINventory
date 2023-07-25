d2919fe68e76176ecc2d7ce39b3488212d529c39
Murphey Osmundson <mosmundson88@gmail.com>
2023-07-24 22:57:38 -0400

    update to AutomobileVO vin field

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -8,7 +8,7 @@ class Technician(models.Model):
 
 
 class AutomobileVO(models.Model):
-    vin = models.CharField(max_length=100, unique=True)
+    vin = models.CharField(max_length=17, unique=True)
     sold = models.BooleanField(default=False)
 
 
