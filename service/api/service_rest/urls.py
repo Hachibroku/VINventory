81fecc262ed6053002687bc52311933afc667c6c
Murphey Osmundson <mosmundson88@gmail.com>
2023-07-27 16:48:27 -0400

    url pathing bugfix

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -3,9 +3,9 @@ from .views import api_technicians, api_technician, api_appointments, api_appoin
 
 urlpatterns = [
     path("technicians/", api_technicians, name="api_technicians"),
-    path("technicians/<int:pk>", api_technician, name="api_technician"),
+    path("technicians/<int:pk>/", api_technician, name="api_technician"),
     path("appointments/", api_appointments, name="api_appointments"),
-    path("appointments/<int:pk>", api_appointment, name="api_appointment"),
+    path("appointments/<int:pk>/", api_appointment, name="api_appointment"),
     path("appointments/<int:pk>/cancel/", api_cancel_appointment, name="api_cancel_appointment"),
     path("appointments/<int:pk>/finish/", api_finish_appointment, name="api_finish_appointment"),
 ]
